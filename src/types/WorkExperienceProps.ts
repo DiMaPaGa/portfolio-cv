60db3ec33f6de4f6232d77178b91cc6db83d24d4
DiMaPaGa <145446082+DiMaPaGa@users.noreply.github.com>
2024-11-21 06:49:34 +0100

    Primera subida de proyecto

---
+++ src/types/WorkExperienceProps.ts
@@ -0,0 +1,10 @@
+export interface WorkExperienceProps {
+    title: string;
+    items: Array<{
+      jobTitle: string;
+      company: string;
+      years: string;
+      description: string;
+    }>;
+  }
+  
